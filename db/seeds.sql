0ae280e03b48ed9fe78104bdc24764a0475633b2
James <joshua.james918@gmail.com>
2023-11-23 03:40:53 -0600

    add departments and all departments

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,10 +2,12 @@ INSERT INTO department (id, department_name)
 VALUES (1, 'Aerospace'),
        (2, 'Science'),
        (3, 'Entertainment');
+
 INSERT INTO role (id, title, salary, department_id)
 VALUES (1, 'Supply Chain Manager', 159874, 1),
        (2, 'Biochemist designer', 147852, 2),
        (3, 'Supply Chain Specialist', 150000, 1);
+
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES (1, 'Clark', 'Kent', 1, 1),
        (2, 'Hal', 'Jordan', 2, 2),

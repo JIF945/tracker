c5dcdcfc4f0db63a7d0473891abc2094a545b7bb
James <joshua.james918@gmail.com>
2023-11-23 00:41:40 -0600

    seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,35 +1,12 @@
--- Schema source department 
-INSERT INTO department (id, department_id)
-VALUES ("Aerospace"),
-        ("science"),
-        ("Entertainment"),
-
-
--- Schema Source for the role 
-INSERT INTO role (title, salary, department_id)
-VALUES ("Suppply Chain Manager", 159874, 1),
-       ('Supply Chain Specialist', 150000, 1),
-       ('Biochemist designer', 147852, 2),
-       ('Biochemist Manager', 189654, 2),
-       ('Singer', 15000, 3),
-       ('Backup Dancer', 12000, 3),
-       ('Marketing Manager', 145632, 4),
-       ('Marketing Associate', 12365, 4)
-       ('Dog Walker', 15632, 5),
-       ('Graphic designer', 89654,2)
-
-
-
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Clark', 'Kent', 1, 1)
-       ('Hal', 'Jordan', 2, null),
-       ('Peter', 'Parker', 3, 2)
-       ('Miles', 'Morales', 4, 3 ),
-       ('Wade', 'Wilson', 5, null),
-       ('Lex', 'Luthor', 6, null),
-       ('Diana', 'Prince', 7, 4),
-       ('Bruce', 'Wayne', 8, null)
-       ('Arthur', 'Curry', 9, null)
-       ('Barry', 'Allen', 10, null)  
-
+INSERT INTO department (id, department_name)
+VALUES (1, 'Aerospace'),
+       (2, 'Science'),
+       (3, 'Entertainment');
+INSERT INTO role (id, title, salary, department_id)
+VALUES (1, 'Supply Chain Manager', 159874, 1),
+       (2, 'Biochemist designer', 147852, 2),
+       (3, 'Supply Chain Specialist', 150000, 1);
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (1, 'Clark', 'Kent', 1, 1),
+       (2, 'Hal', 'Jordan', 2, 2),
+       (3, 'Peter', 'Parker', 3, 3);

9270e271eb665c61aa23bebcd05764ee684c7ca1
James <joshua.james918@gmail.com>
2023-11-22 23:17:17 -0600

    whatever

--- db/schema.sql
+++ db/schema.sql
@@ -1,33 +1,24 @@
--- drop and create, if database exist drop it, db doesnt exist create the db.
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
 
-DROP DATABASE IF EXISTS employees_db;
+USE employee_db;
 
-CREATE DATABASE employees_db;
-
-
-
--- table for department
-CREATE TABLE department (
-    id INT PRIMARY KEY,
-    department_name VARCHAR(30),
+CREATE TABLE department(
+  id INT PRIMARY KEY,
+  department_name VARCHAR(30)
 );
 
--- Table for role
-CREATE TABLE role (
-    id INT PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL,
-    department_id INT,
-    
+CREATE TABLE role(
+  id INT PRIMARY KEY,
+  title VARCHAR(30),
+  department_id INT,
+  salary DECIMAL
 );
 
--- Table for employee
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT NOT NULL,
-    manager_id INT,
-    
+  id INT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT
 );
-

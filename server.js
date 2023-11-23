bd61a4c2e02b0990fecb7c1da96c626da3b7d2b3
James <joshua.james918@gmail.com>
2023-11-23 03:42:19 -0600

    removed password from connection

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "jojames315$",
+  password: "",
   database: "employee_db",
 });
 

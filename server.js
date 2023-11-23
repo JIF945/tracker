382bbb24830611d289b6cb7611df067cb0e3e54a
James <joshua.james918@gmail.com>
2023-11-22 20:00:43 -0600

    function to view employees

--- server.js
+++ server.js
@@ -250,3 +250,24 @@ async function deleteRole (){
         init();
     }
 }
+
+//  function to view employees
+
+async function viewEmployees (){
+    try {
+        const [rows, fields] = await db.promise().query(`
+        SELECT e.id, e.first_name, e.last_name, r.title AS title, d.name AS department, r.salary, CONCAT(m.first_name,"", m.last_name) AS manager
+        FROM employee e
+        LEFT JOIN role r ON e role_id = r.id
+        LEFT JOIN department d ON r department_id = d.id
+        LEFT JOIN employee m ON e.manger_id = m.id`);
+
+        console.log('Displaying employees.');
+        console.table(rows);
+        init();
+    } catch (error) {
+        console.error('error, cant show employees', error);
+        init();
+    }
+}
+
